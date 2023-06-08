e1d520b51eee21c82cd6cb4de015d8f58c116fb0
Philippe Antoine <contact@catenacyber.fr>
2023-06-08 15:39:29 +0200

    quadfuzz: restrict used entropy to 16 bits

--- quadfuzz.c
+++ quadfuzz.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -8,6 +9,17 @@ size_t LLVMFuzzerMutate(uint8_t *Data, size_t size, size_t MaxSize);
 
 static bool quad_fuzz_init = false;
 static bool quad_fuzz_debug = false;
+
+uint32_t pattern_repeat_nbs[8] = {
+    0x200,
+    0xFFF,
+    0x1000,
+    0x1001,
+    0xFFFF,
+    0x10000,
+    0x10001,
+    0x20000,
+};
 
 size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t size, size_t MaxSize, unsigned int Seed)
 {
@@ -22,18 +34,18 @@ size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t size, size_t MaxSize, unsig
         return LLVMFuzzerMutate(Data, size, MaxSize);
     }
     Seed = Seed >> 1;
-    uint8_t pattern_repeat_len = 1 + (Seed & 0xF);
+    size_t pattern_repeat_len = 1 + (Seed & 0xF);
     Seed = Seed >> 4;
-    uint32_t pattern_repeat_nb = 512 * ((Seed & 0xFF) + 1);
-    Seed = Seed >> 8;
-    uint16_t prefix_postfix_ratio = (Seed & 0xFFFF);
-    Seed = Seed >> 16;
+    size_t pattern_repeat_nb = pattern_repeat_nbs[Seed & 0x7];
+    Seed = Seed >> 3;
+    size_t prefix_postfix_ratio = (Seed & 0x7F);
+    Seed = Seed >> 7;
 
     if (pattern_repeat_len > size) {
         pattern_repeat_len = size;
     }
     size_t prefix_postfix_len = size - pattern_repeat_len;
-    size_t prefix_len = (prefix_postfix_len * prefix_postfix_ratio) / 0xFFFF;
+    size_t prefix_len = (prefix_postfix_len * prefix_postfix_ratio) / 0xFF;
     size_t postfix_len = prefix_postfix_len - prefix_len;
     size_t qsize = prefix_len + (pattern_repeat_nb * pattern_repeat_len) + postfix_len;
     if (qsize > MaxSize) {
@@ -44,9 +56,9 @@ size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t size, size_t MaxSize, unsig
         printf("size = %zu\n", size);
         printf("MaxSize = %zu\n", MaxSize);
         printf("Seed = %u\n", Seed);
-        printf("pattern_repeat_len = %hhu\n", pattern_repeat_len);
-        printf("pattern_repeat_nb = %u\n", pattern_repeat_nb);
-        printf("prefix_postfix_ratio = %hu\n", prefix_postfix_ratio);
+        printf("pattern_repeat_len = %zu\n", pattern_repeat_len);
+        printf("pattern_repeat_nb = %zu\n", pattern_repeat_nb);
+        printf("prefix_postfix_ratio = %zu\n", prefix_postfix_ratio);
         printf("prefix_postfix_len = %zu\n", prefix_postfix_len);
         printf("prefix_len = %zu\n", prefix_len);
         printf("postfix_len = %zu\n", postfix_len);
